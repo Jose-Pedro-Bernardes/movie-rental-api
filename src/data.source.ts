250622f5715b7ca64b90a0efe8605a1923a14213
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-05-01 22:58:39 -0300

    feat: configurando o data.source.ts

---
+++ src/data.source.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import path from "path";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+const DataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "entities/**.{ts,js}");
+  const migrationsPath: string = path.join(__dirname, "migrations/**.{ts,js}");
+
+  if (process.env.NODE_ENV === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  const dbUrl = process.env.DATABASE_URL?.toString();
+
+  if (!dbUrl) {
+    throw new Error("Missing env var: 'DATABASE_URL'");
+  }
+
+  return {
+    type: "postgres",
+    url: dbUrl,
+    synchronize: false,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const AppDataSource: DataSource = new DataSource(DataSourceConfig());
+export { AppDataSource };
